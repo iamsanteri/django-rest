e6de38bb3ff7dc2ce8ea64a060bae38c148ad67e
iamsanteri <alexander.liukkonen@gmail.com>
2024-02-25 22:03:23 +0200

    Finished implementing image upload functionality

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -20,5 +20,15 @@ class RecipeDetailSerializer(RecipeSerializer):
     """ Serializer for recipe details. """
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ["description"]
+        fields = RecipeSerializer.Meta.fields + ["description", "image"]
         read_only_fields = RecipeSerializer.Meta.read_only_fields
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """ Serializer for uploading images to recipes. """
+
+    class Meta:
+        model = Recipe
+        fields = ["id", "image"]
+        read_only_fields = ["id"]
+        extra_kwargs = {"image": {"required": "True"}}
