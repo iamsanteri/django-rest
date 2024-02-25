766022a173e28436f0096b3c5e9f535b921515fd
iamsanteri <alexander.liukkonen@gmail.com>
2024-02-25 13:59:30 +0200

    Implemented recipe API.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,11 +15,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Recipe.objects.all()
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
 
     def get_queryset(self):
         """ Return objects for the current authenticated user only. """
         return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """ Return appropriate serializer class. """
+        if self.action == "list":
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
 
     def perform_create(self, serializer):
         """ Create a new recipe. """
