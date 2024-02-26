1a1011ededab2fc4ce576e85d4bebb3228d89162
iamsanteri <alexander.liukkonen@gmail.com>
2024-02-26 21:16:14 +0200

    Added health checking API.

---
+++ app/core/tests/test_health_check.py
@@ -0,0 +1,21 @@
+"""
+Tests for the health check API.
+"""
+
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class HealthCheckTests(TestCase):
+    """ Test the health check API. """
+
+    def test_health_check(self):
+        """ Test the health check API. """
+        url = reverse('health-check')
+        client = APIClient()
+        res = client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
